f7fc9405ff02c5b517e21dbaddc81ce949439a1a
RAVURI SREE SAI HARI KRISHNA <RAVURISREESAIHARIKRISHNA@users.noreply.github.com>
2017-06-17 16:11:09 +0530

    Add files via upload

---
+++ ScreenChangeDemo/src/screenchangedemo/NextPageController.java
@@ -0,0 +1,9 @@
+package screenchangedemo;
+
+import javafx.event.ActionEvent;
+
+public class NextPageController {
+    public void Hello(ActionEvent event){
+        System.out.println("Hello World");
+    }
+}
